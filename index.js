a171d83427bd3e514408b596d2ef7c9ba23a34a3
omkeshborse <bomkesh25@gmail.com>
2024-01-27 13:44:51 +0530

    feat: Implement task insertion function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Kanban from "./kanban.js ";
 
 // console.log(Kanban.getAllTasks() );
-console.log(Kanban.getTasks(2));
+// console.log(Kanban.getTasks(2));
+// console.log(Kanban.insertTask(1, "Edit kanban Project lecture")); 
--- kanban.js
+++ kanban.js
@@ -1,31 +1,45 @@
 export default class Kanban {
   static getTasks(columnId) {
-    const data = read().find(column=>column.columnId ===columnId ) ;
+    const data = read().find((column) => column.columnId === columnId);
     if (!data) {
-      return [] ;
+      return [];
     }
-    return data.tasks ;
+    return data.tasks;
   }
-  static insertTask(columnId, content) {}
+  static insertTask(columnId, content) {
+    const data = read();
+    const column = data.find((column) => column.columnId === columnId);
+
+    const task = {
+      taskId: Math.floor(Math.random() * 100000),
+       content: content,
+    };
+    if (!column) {
+      throw new Error("Column doesn't exists !")
+    }
+    column.tasks.push(task);
+    console.log(data);
+    localStorage.setItem("data" , JSON.stringify(data)) ;
+    return task ;
+  }
   static updateTask(taskId, updatedInfo) {}
   static deleteTask(taskId) {}
   static getAllTasks() {
-    const data = read() ;
+    const data = read();
     return [data[0].tasks, data[1].tasks, data[2].tasks];
   }
 }
 
 function read() {
-    const data = localStorage.getItem("data")  ;
-    if (!data) {
-      return [
-        { columnId: 0, tasks: [] },
-        { columnId: 1, tasks: [] },
-        { columnId: 2, tasks: [] },
-      ];
-    }
-    return JSON.parse(data) ;
+  const data = localStorage.getItem("data");
+  if (!data) {
+    return [
+      { columnId: 0, tasks: [] },
+      { columnId: 1, tasks: [] },
+      { columnId: 2, tasks: [] },
+    ];
+  }
+  return JSON.parse(data);
 }
 
 function save() {}
- 
